df52fd816357721fa12bfbaf276a6ce12daa16ff
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-10-31 18:00:32 +0700

    change

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -1,53 +1,69 @@
 <!DOCTYPE html>
-<html >
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Login</title>
-  
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<title>Branded Login Form Responsive Widget Template :: w3layouts</title>
+<!-- Meta tag Keywords -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Branded Login Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Meta tag Keywords -->
+<!-- css files -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+<link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+<!-- //css files -->
+<!-- online-fonts -->
+<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<!-- //online-fonts -->
 
-      <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
-      <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-
-      <link rel="stylesheet" href="css/style.css">
-
-  
 </head>
-
 <body>
-  
-
-<div class="pen-title">
-  <h1>OConnect</h1>
-</div>
-<div class="container">
-  <div class="card"></div>
-  <div class="card">
-    <h1 class="title">Login</h1>
-    <form>
-      <div class="input-container">
-        <input type="#{type}" id="#{label}" required="required"/>
-        <label for="#{label}">Username</label>
-        <div class="bar"></div>
+<div class="w3-agile-banner">
+  <div class="center-container">
+    <!--header-->
+    <div class="header-w3l">
+      <h1>Branded Login Form</h1>
+    </div>
+    <!--//header-->
+    <!--main-->
+    <div class="main-content-agile">
+      <div class="wthree-pro">
+        <h2>Login Now</h2>
       </div>
-      <div class="input-container">
-        <input type="#{type}" id="#{label}" required="required"/>
-        <label for="#{label}">Password</label>
-        <div class="bar"></div>
+      <div class="sub-main-w3"> 
+        <form action="#" method="post">
+          <input placeholder="E-mail" name="mail" type="email" required="">
+          <span class="icon1"><i class="fa fa-user" aria-hidden="true"></i></span>
+          <input  placeholder="Password" name="Password" type="password" required="">
+          <span class="icon2"><i class="fa fa-unlock" aria-hidden="true"></i></span>
+          <div class="rem-w3">
+            <span class="check-w3"><input type="checkbox" />Remember Me</span>
+            <a class="w3-pass" href="#">Forgot Password?</a>
+            <div class="clear"></div>
+          </div>
+          <div class="w3-head-continue">
+            <h4>Continue with</h4>
+          </div>
+          <div class="navbar-right social-icons"> 
+              <ul>
+                <li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+                <li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+                <li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+                <li><a href="#" class="fa fa-pinterest icon-border rss"> </a></li>
+              </ul>  
+            </div>
+          <input type="submit" value="Login">
+        </form>
       </div>
-      <div class="button-container">
-        <button><span>LOGIN</span></button>
-      </div>
-      <div class="footer"><a href="#">Reset Password</a></div>
-      <div class="footer"><a href="/activate">Go To Activate (For Testing)</a></div>
-    
-    </form>
+    </div>
+    <!--//main-->
+    <!--footer-->
+    <div class="footer">
+      <p>&copy; 2017 Branded Login Form. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+    </div>
+    <!--//footer-->
   </div>
-  
-    
-  </div>
 </div>
-
- 
 </body>
 </html>
