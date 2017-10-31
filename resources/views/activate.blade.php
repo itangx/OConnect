6a91620be04d2bd7cae0135bfbd8cff197c38374
falcon24092 <b____ank@hotmail.com>
2017-10-31 21:29:19 +0700

    add Activated page command

--- resources/views/activate.blade.php
+++ resources/views/activate.blade.php
@@ -10,13 +10,16 @@
 <body>
     <div class="login-page">
         <div class="form">
-          <form class="login-form">
+          <form class="login-form" action="/activated" method="post">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <p class="topic">Activated User</p><br>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Old Password" />
-            <input type="password" placeholder="Re-Old Password" />
-            <input type="password" placeholder="New Password" />
-            <input type="password" placeholder="Re-New Password" />
+            <input type="text" name="username" placeholder="Username" value="{{ old('username') }}" />
+            <input type="password" name="oldpassword" placeholder="Old Password" />
+            <input type="password" name="newpassword1" placeholder="New Password" />
+            <input type="password" name="newpassword2" placeholder="Re-New Password" />
+            @if(isset($msg))
+            <p style="color:red">{{$msg}}</p>
+            @endif
             <button>Submit</button>
             <p class="message"><a href="/">Back to Login Page (For Testing)</a></p>
         </form>
