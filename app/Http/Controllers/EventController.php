18398c5e8dd532af2c3681baeaf77ffb8383915e
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-11-09 00:42:02 +0700

    change db

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -17,9 +17,11 @@ class EventController extends Controller
 	public function search($eventId,Request $request){
 		$event = Event::where('event_id',$eventId)->get();
 		$buyer = DB::table('SEARCH_BUYER')
-					->where('person_name','like','%THA%')
-					->orWhere('corp_name','like','%นิล%')
 					->where('ID','=','2')
+					->where(function ($query) {
+						$query->where('person_name','like','%THA%')
+							  ->orWhere('corp_name','like','%นิล%');
+							})
 					// ->groupBy('person_name','corp_name','person_mob','person_email','corp_per_rel_id')	
 					// ->orderBy('ID')
 					->get();
